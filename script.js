e3b8aaa0172a742d5ca0dcc118f73cbdec0ba83d
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-18 01:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+document.querySelector("input[type='button']").onclick = function () {
+    let select = document.getElementById("colorSelect");
+    if (select.selectedIndex !== -1) {
+        select.remove(select.selectedIndex);
+    }
+};
