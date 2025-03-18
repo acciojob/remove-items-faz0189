f8300ca37486bd1468f536284ae922cefeee1cf7
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-18 13:43:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-function remove_color(){
-	let removalcolor = document.getElementById("colorSelect")
-	let button = document.getquerySelector("input[type='button']")
+function remove_color() {
+    let removalcolor = document.getElementById("colorSelect"); // Get the select element
+    let button = document.querySelector("input[type='button']"); // Get the button
 
-	button.addEventListener("click" function() {
-		if(select.selectedIndex! == -1){
-			select.remove(select.selectedIndex)
-			
-		}
-	});
-	
+    button.addEventListener("click", function () {
+        if (removalcolor.selectedIndex !== -1) { // Check if an option is selected
+            removalcolor.remove(removalcolor.selectedIndex); // Remove selected option
+        }
+    });
 }
